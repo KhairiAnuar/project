738e8f803ba4dbd147b10e85da1258c1707c3525
Khairi <Khairianuar164@gmail.com>
2019-11-21 16:13:31 +1000

    Add neccessary comments and bigger web search pagination

--- index.php
+++ index.php
@@ -1,52 +1,57 @@
-<!--Khairi project summarization-->
+<!--
+Author: Khairi Anuar
+Project: Web searching with summarization
+-->
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>MyGoogle Search</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="website icon" href="assets/images/favicon.ico" type="image/x-icon"/>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <!--To make icons from fontawesome loads faster-->
-        <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
-        <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="assets/scripts/app.js"></script>
-        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<head>
+    <title>MyGoogle Search</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="website icon" href="assets/images/favicon.ico" type="image/x-icon"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/ac2d020881.js" crossorigin="anonymous"></script>
+    <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="assets/scripts/app.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-    </head>
+</head>
 
-    <body>
-    <div class="container-fluid h-100">
-        <div class="row align-items-center h-100">
-            <div class="col mx-auto d-flex flex-column align-items-center">
-                <img src="assets/images/mygoogle.png" id="logo" class="w-22" alt="Logo Website"><br>
-                <div class="row align-items-center">
-                    <div class="col col-md-auto">
-                        <form class="indexForm" action="search.php" method="get">
-                            <div class="input-group show-idle-prompt">
-                                <div class="queryDiv form-group d-inline-flex ui-widget" style="margin-bottom: 0">
-                                    <select class="custom-select border-secondary border-right-0" id="querySelect" name="type">
-                                        <option value="news" selected>News</option>
-                                        <option value="wikipedia">Wikipedia</option>
-                                    </select>
-                                    <input type="text" placeholder="type here" size="50" autofocus class="border-right-0 rounded-0 form-control w-100" id="query" name="query" aria-label="input search query"/>
+<body>
+<div class="container-fluid h-100">
+    <div class="row align-items-center h-100">
+        <div class="col mx-auto d-flex flex-column align-items-center">
+            <img src="assets/images/mygoogle.png" id="logo" class="w-22" alt="Logo Website"><br>
+            <div class="row align-items-center">
+                <div class="col col-md-auto">
+                    <form class="indexForm" action="search.php" method="get">
+                        <div class="input-group show-idle-prompt">
+                            <div class="queryDiv form-group d-inline-flex ui-widget" style="margin-bottom: 0">
+                                <select class="custom-select border-secondary border-right-0" id="querySelect"
+                                        name="type">
+                                    <option value="news" selected>News</option>
+                                    <option value="wikipedia">Wikipedia</option>
+                                </select>
+                                <input type="text" placeholder="type here" size="50" autofocus
+                                       class="border-right-0 rounded-0 form-control w-100" id="query" name="query"
+                                       aria-label="input search query"/>
 
-                                    <div class="input-group-append">
-                                        <input type="hidden" name="pageno" value="1">
-                                        <button id="btnSearch" class="btn btn-outline-secondary" value="Search"  type="submit"><i class="fas fa-search"> </i> <span >Search</span></button>
-                                    </div>
-
+                                <div class="input-group-append">
+                                    <input type="hidden" name="pageno" value="1">
+                                    <button id="btnSearch" class="btn btn-outline-secondary" value="Search"
+                                            type="submit"><i class="fas fa-search"> </i> <span>Search</span></button>
                                 </div>
                             </div>
-                        </form>
-                    </div>
-           </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
+</div>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-    </body>
+</body>
 </html>
--- savePage.php
+++ savePage.php
@@ -1,22 +1,30 @@
 <?php
+/**
+ * Webpages saved by HtmlSaveComplete.php http://sarfraznawaz.wordpress.com and cURL
+ * Web manipulations of summarized webpage and original webpage handle by simple_html_dom.php
+ * PHP Simple HTML DOM Parser https://simplehtmldom.sourceforge.io/
+ *
+**/
+
 # include the files
 require_once 'getsummary.php';
 require_once 'assets/savePage/simple_html_dom.php';
 require 'assets/savePage/htmlSaveComplete.php';
 
 $contentonly = true;
-$allContent=false;
+$allContent = false;
 $keepjs = false;
 $compress = true;
-    //Remove sentences from array summary over specified number characters
-    foreach ($summary as $key=>$oneSentence){
-        if (strlen($oneSentence)<30 || strlen($oneSentence) >220 ){
-            unset($summary[$key]);
-        }
 
-
+//Remove sentences from array summary that greater than specified number characters
+foreach ($summary as $key => $oneSentence) {
+    if (strlen($oneSentence) < 30 || strlen($oneSentence) > 220) {
+        unset($summary[$key]);
     }
-function file_get_contents_curl($url) {
+}
+
+function file_get_contents_curl($url)
+{
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
@@ -25,10 +33,10 @@ function file_get_contents_curl($url) {
     curl_close($ch);
     return $data;
 }
-if(strpos($url,'kidsnews')){
-        //Below removes unrelated sentences or duplicated words
-    foreach($summary as $key => $oneSentence) {
 
+//Below removes unrelated sentences or duplicated words
+if (strpos($url, 'kidsnews')) {
+    foreach ($summary as $key => $oneSentence) {
         if (strpos($oneSentence, 'HAVE YOUR SAY:') !== false) {
             unset($summary[$key]);
         }
@@ -41,173 +49,183 @@ if(strpos($url,'kidsnews')){
         if (strpos($oneSentence, 'Curriculum Links:') !== false) {
             unset($summary[$key]);
         }
-        if (strpos($oneSentence,'Write a diary entry')!==false){
+        if (strpos($oneSentence, 'Write a diary entry') !== false) {
             unset($summary[$key]);
         }
 
     }
-    $html=mb_convert_encoding(file_get_contents_curl($url),"HTML-ENTITIES","UTF-8");
+    $html = mb_convert_encoding(file_get_contents_curl($url), "HTML-ENTITIES", "UTF-8");
     $sumhtml = str_get_html($html);
-    $originhtml=file_get_contents_curl($url);
-    $orihtml= str_get_html($originhtml);
+    $originhtml = file_get_contents_curl($url);
+    $orihtml = str_get_html($originhtml);
 
 
-    } else if(strpos($url,'wiki')){
-        $htmlSaveComplete = new htmlSaveComplete($url);
-        $originhtml=$htmlSaveComplete->getCompletePage($keepjs, $allContent, $compress);
-        $html = $htmlSaveComplete->getCompletePage($keepjs, $contentonly, $compress);
-        $sumhtml = str_get_html($html);
-        $orihtml= str_get_html($originhtml);
+} else if (strpos($url, 'wiki')) {
+    $htmlSaveComplete = new htmlSaveComplete($url);
+    $originhtml = $htmlSaveComplete->getCompletePage($keepjs, $allContent, $compress);
+    $html = $htmlSaveComplete->getCompletePage($keepjs, $contentonly, $compress);
+    $sumhtml = str_get_html($html);
+    $orihtml = str_get_html($originhtml);
 
-        }else{ echo 'Sorry website is not available';}
+} else {
+    echo 'Sorry website is not available';
+}
 
 //For collapsible div summary toggle
-$splitsum1=array_slice($summary,0,3);
-$splitsum2=array_slice($summary,4,1);
-$splitsum3=array_slice($summary,5);
+$splitsum1 = array_slice($summary, 0, 3);
+$splitsum2 = array_slice($summary, 4, 1);
+$splitsum3 = array_slice($summary, 5);
 
+/**
+ * Summarized Webpage HTML Modification*
+ */
 
 //Summarized Wikipedia webpage
-if (strpos( $url, 'wiki')) {
-    $sumhtml->find('head',0)->innertext.='<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
-    $sumhtml->find('head',0)->innertext .='<link rel="stylesheet" href="assets/css/collapse.css">';
-    $sumhtml->find('head',0)->innertext .='<link rel="stylesheet" type="text/css" href="assets/css/wikipedia.css">';
+if (strpos($url, 'wiki')) {
+    $sumhtml->find('head', 0)->innertext .= '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+    $sumhtml->find('head', 0)->innertext .= '<link rel="stylesheet" href="assets/css/collapse.css">';
+    $sumhtml->find('head', 0)->innertext .= '<link rel="stylesheet" type="text/css" href="assets/css/wikipedia.css">';
 
-    $sumhtml->find('head',0)->innertext .='<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
-    $sumhtml->find('style',0)->innertext.='.heroimg{
+    $sumhtml->find('head', 0)->innertext .= '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
+    $sumhtml->find('style', 0)->innertext .= '.heroimg{
     max-width: 400px;
     width: 100%;
 }';
 
-    if ($sumhtml->find('div[class=mw-parser-output]',0)){
+    if ($sumhtml->find('div[class=mw-parser-output]', 0)) {
         //If hero image available
-        if(!empty($extract->image)) {
-            $sumhtml->find('div[class=mw-parser-output]', 0)->innertext ='</h1> <img class="heroimg" src="' . $extract->image . '" alt="Hero image"><br><br><p>' . implode('</p><p>', $splitsum1) . '</p><button class="collapsible"><p>'.$splitsum2[0].'</p></button><div class="content">
-                <p>'. implode('</p><p>', $splitsum3).'</p></div>';
+        if (!empty($extract->image)) {
+            $sumhtml->find('div[class=mw-parser-output]', 0)->innertext = '</h1> <img class="heroimg" src="' . $extract->image . '" alt="Hero image"><br><br><p>' . implode('</p><p>', $splitsum1) . '</p><button class="collapsible"><p>' . $splitsum2[0] . '</p></button><div class="content">
+                <p>' . implode('</p><p>', $splitsum3) . '</p></div>';
 
-        }else{
-            $sumhtml->find('div[class=mw-parser-output]', 0)->innertext = '<br><p>' . implode('</p><p>', $splitsum1) . '</p><button class="collapsible"><p>'.$splitsum2[0].'</p></button><div class="content">
-                <p>'. implode('</p><p>', $splitsum3).'</p></div>';
+        } else {
+            $sumhtml->find('div[class=mw-parser-output]', 0)->innertext = '<br><p>' . implode('</p><p>', $splitsum1) . '</p><button class="collapsible"><p>' . $splitsum2[0] . '</p></button><div class="content">
+                <p>' . implode('</p><p>', $splitsum3) . '</p></div>';
         }
 
 
-        if($sumhtml->find('body',0)){
-            $sumhtml->find('body',0)->outertext='<div class="container"><div class="row"><div class="col-8">'.$sumhtml->find('#content',0)->outertext.'</div><div class="col-4"><script type="text/javascript" src="assets/scripts/collapse.js" ></script> <div class="mw-stack clickableBigger" style="padding:100px 50px 100px 50px;cursor:pointer;  box-sizing:border-box;float:right; clear:right;"  onclick="window.location.href = \'origin.html\'">
+        if ($sumhtml->find('body', 0)) {
+            $sumhtml->find('body', 0)->outertext = '<div class="container"><div class="row"><div class="col-8">' . $sumhtml->find('#content', 0)->outertext . '</div><div class="col-4"><script type="text/javascript" src="assets/scripts/collapse.js" ></script> <div class="mw-stack clickableBigger" style="padding:100px 50px 100px 50px;cursor:pointer;  box-sizing:border-box;float:right; clear:right;"  onclick="window.location.href = \'origin.html\'">
                                                               <a class="btn btn-secondary btn-lg btnSwitch" style="padding: 1rem 1rem; text-decoration: none" href="origin.html"><h3 style=" color: white;">Go original page</h3></a> 
                                                                 </div></div></div></div>';
         }
     }
-}
-//Summarized KidsNews Webpage
-else if(strpos($url,'kidsnews')){
+} //Summarized KidsNews Webpage
+else if (strpos($url, 'kidsnews')) {
 
-    if (!empty($sumhtml)){
-        $sumhtml->find('head',0)->innertext .='<link rel="stylesheet" href="assets/css/Knews.css"><link rel="stylesheet"
+    if (!empty($sumhtml)) {
+        $sumhtml->find('head', 0)->innertext .= '<link rel="stylesheet" href="assets/css/Knews.css"><link rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
 
-        $sumhtml->find('head',0)->innertext .='<link rel="stylesheet" href="assets/css/collapse.css">';
-        $sumhtml->find('style',0)->innertext .='.image{ max-width:930px}';
+        $sumhtml->find('head', 0)->innertext .= '<link rel="stylesheet" href="assets/css/collapse.css">';
+        $sumhtml->find('style', 0)->innertext .= '.image{ max-width:930px}';
 
-        if ($sumhtml->find('.header',0)){
-            $sumhtml->find('.logo',0)->outertext='';
-            $sumhtml->find('.fixed-nav-container.row',0)->outertext='';
-            $sumhtml->find('.header',0)->innertext .='<script src="assets/scripts/collapse.js" type="text/javascript"></script>';
-           }
-        if($sumhtml->find('#single',0 )) {
+        if ($sumhtml->find('.header', 0)) {
+            $sumhtml->find('.logo', 0)->outertext = '';
+            $sumhtml->find('.fixed-nav-container.row', 0)->outertext = '';
+            $sumhtml->find('.header', 0)->innertext .= '<script src="assets/scripts/collapse.js" type="text/javascript"></script>';
+        }
+        if ($sumhtml->find('#single', 0)) {
             $head = $sumhtml->find('.headline', 0);
             $sumhtml->find('#single', 0)->innertext = '<div id="info"><h1>' . $head . '</h1><div class="text-right" onclick="window.location.href = \'origin.html\'">
                                                                        <a class="btn btn-secondary btn-lg btnSwitch" style="padding: 1rem 1rem; margin-bottom:10px; " href="origin.html"><h4 >Go original page</h4></a> 
                                                                         </div> <img class="image" src="' . $extract->image . '" alt="Hero image"></div>';
-            $sumhtml->find('.meta',0)->outertext='';
-              $sumhtml->find('#single',0)->innertext.='<div id="story" style="display:block"><p></p>'
-                  . '<p class="capi-html">' . implode('</p><p class="capi-html">', $splitsum1) . '</p><button class="collapsible"><p class="capi-html">'.$splitsum2[0].'</p></button><div class="content"><p class="capi-html">
-                   '. implode('</p><p class="capi-html">', $splitsum3).'</p></div></div>';
+            $sumhtml->find('.meta', 0)->outertext = '';
+            $sumhtml->find('#single', 0)->innertext .= '<div id="story" style="display:block"><p></p>'
+                . '<p class="capi-html">' . implode('</p><p class="capi-html">', $splitsum1) . '</p><button class="collapsible"><p class="capi-html">' . $splitsum2[0] . '</p></button><div class="content"><p class="capi-html">
+                   ' . implode('</p><p class="capi-html">', $splitsum3) . '</p></div></div>';
 
         }
-        $sumhtml->find('#module-more-in',0)->outertext='';
-        if ($sumhtml->find('#footer',0)){
-            $sumhtml->find('#footer',0)->outertext='';
+        $sumhtml->find('#module-more-in', 0)->outertext = '';
+        if ($sumhtml->find('#footer', 0)) {
+            $sumhtml->find('#footer', 0)->outertext = '';
         }
-        $sumhtml->find('#page',0)->innertext.='<script src="assets/scripts/collapse.js" type="text/javascript"></script>';
+        $sumhtml->find('#page', 0)->innertext .= '<script src="assets/scripts/collapse.js" type="text/javascript"></script>';
 
-    }else{ echo 'Sorry page is empty';}
+    } else {
+        echo 'Sorry page is empty';
+    }
+} else {
+    echo 'Website specified not applicable to save ';
 }
-else{
-    echo'Website specified not applicable to save ';
-}
-//Original contents html modification
-//Wikipedia
-if (strpos( $url, 'wiki')) {
-    $orihtml->find('head',0)->innertext .='<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
-    $orihtml->find('head',0)->innertext .='<link rel="stylesheet" type="text/css" href="assets/css/wikipedia.css">';
-    if($orihtml->find('div[id=mw-navigation]',0)){
-        $orihtml->find('div[id=mw-head]',0)->outertext='';
-        foreach ($orihtml->find('div[class=portal]') as $sideMenu){
-            $sideMenu->outertext='';
+
+/**
+ * Original Webpage HTML Modification
+ */
+
+//Wikipedia original webpage
+if (strpos($url, 'wiki')) {
+    $orihtml->find('head', 0)->innertext .= '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+    $orihtml->find('head', 0)->innertext .= '<link rel="stylesheet" type="text/css" href="assets/css/wikipedia.css">';
+    if ($orihtml->find('div[id=mw-navigation]', 0)) {
+        $orihtml->find('div[id=mw-head]', 0)->outertext = '';
+        foreach ($orihtml->find('div[class=portal]') as $sideMenu) {
+            $sideMenu->outertext = '';
         }
     }
-    if ($orihtml->find('#content',0)){
-        $orihtml->find('#catlinks',0)->outertext='';
-        $orihtml->find('#toc',0)->outertext='';
+    if ($orihtml->find('#content', 0)) {
+        $orihtml->find('#catlinks', 0)->outertext = '';
+        $orihtml->find('#toc', 0)->outertext = '';
 
     }
-    if($orihtml->find('.navbox',0)){$orihtml->find('.navbox',0)->outertext=''; $orihtml->find('#mw-navigation',0)->outertext='';}
-    if($orihtml->find('#content',0)){
-
-        foreach ($orihtml->find('.mw-jump-link')as $brokenJumpLinks){
-            $brokenJumpLinks->outertext='';
+    if ($orihtml->find('.navbox', 0)) {
+        $orihtml->find('.navbox', 0)->outertext = '';
+        $orihtml->find('#mw-navigation', 0)->outertext = '';
+    }
+    if ($orihtml->find('#content', 0)) {
+        foreach ($orihtml->find('.mw-jump-link') as $brokenJumpLinks) {
+            $brokenJumpLinks->outertext = '';
         }
     }
 
-    if($orihtml->find('#mw-head-base',0)){
-    $orihtml->find('#mw-head-base',0)->innertext='<div class="mw-stack clickableBigger" style="  box-sizing:border-box;float:right; clear:right; "  onclick="window.location.href = \'summarized.html\'">
-                                                                <!--  <button class="" type="button" style="border-radius:0;">--> <a class="btn btn-secondary btn-lg btnSwitch" style="border-radius: .3rem; text-decoration: none" href="summarized.html"><h4 style="font-size: 100%; color: white;">Go summarized page</h4></a> 
+    if ($orihtml->find('#mw-head-base', 0)) {
+        $orihtml->find('#mw-head-base', 0)->innertext = '<div class="mw-stack clickableBigger" style="  box-sizing:border-box;float:right; clear:right; "  onclick="window.location.href = \'summarized.html\'">
+                                                              <a class="btn btn-secondary btn-lg btnSwitch" style="border-radius: .3rem; text-decoration: none" href="summarized.html"><h4 style="font-size: 100%; color: white;">Go summarized page</h4></a> 
                                                                   </div>';
     }
-}   //KidsNews
-    else if(strpos($url,'kidsnews')){
+}   //KidsNews Original webpage
+else if (strpos($url, 'kidsnews')) {
 
-        if (!empty($orihtml)){
-            $orihtml->find('head',0)->innertext .='<link rel="stylesheet" href="assets/css/Knews.css"><link rel="stylesheet"
+    if (!empty($orihtml)) {
+        $orihtml->find('head', 0)->innertext .= '<link rel="stylesheet" href="assets/css/Knews.css"><link rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
-    /*        $orihtml->find('div[class=btyb]')->outertext='';*/
 
-            if ($orihtml->find('.header',0)){
-                $orihtml->find('.logo',0)->outertext='';
-                $orihtml->find('.fixed-nav-container.row',0)->outertext='';
-             }
-            $orihtml->find('aside[class=rightrail]',0)->outertext='';
-            if($orihtml->find('p[class=intro]',0)){
-                $orihtml->find('p[class=intro]',0)->outertext='<div class="text-right" onclick="window.location.href = \'summarized.html\'">
+
+        if ($orihtml->find('.header', 0)) {
+            $orihtml->find('.logo', 0)->outertext = '';
+            $orihtml->find('.fixed-nav-container.row', 0)->outertext = '';
+        }
+        $orihtml->find('aside[class=rightrail]', 0)->outertext = '';
+        if ($orihtml->find('p[class=intro]', 0)) {
+            $orihtml->find('p[class=intro]', 0)->outertext = '<div class="text-right" onclick="window.location.href = \'summarized.html\'">
                                                                            <a class="btn btn-secondary btn-lg btnSwitch" style="padding: 1rem 1rem; margin-bottom:10px;" href="summarized.html"><h4 >Go summarized page</h4></a> 
                                                                             </div>';
-            }
-            foreach ($orihtml->find('.caption') as $caption){
-                $caption->outertext='';
-            }
+        }
+        foreach ($orihtml->find('.caption') as $caption) {
+            $caption->outertext = '';
         }
     }
-
-    else{
-        echo ' sorry website not applicable to summarize';
-    }
+} else {
+    echo ' sorry website not applicable to summarize';
+}
 
 if (!$html) {
     header('LOCATION: index.php?e=' . urlencode('Error saving the page, please try again later.'));
     exit;
 }
-//Saves modified contents into html files
+
+//Saves modified contents into HTML files
 $orihtml->save('origin.html');
 $sumhtml->save('summarized.html');
+
 //clear memory
 $orihtml->clear();
 unset($orihtml);
 $sumhtml->clear();
 unset($sumhtml);
-/*file_put_contents('origin.html', $orihtml);
-file_put_contents('summarized.html', $outhtml);*/
 
-//redirect
+
+//redirect client to summarized webpage
 header('LOCATION: summarized.html');
 exit;
 
